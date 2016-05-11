c78c0814cb9fbcea60b67bf1ad2cc319a0ec7ce4
Martin Lang <martin@mercator.io>
2016-05-11 14:37:22 -0700

    moving dirs

--- tasks/typescript.js
+++ tasks/typescript.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const flatten = require('gulp-flatten');
 const sourcemaps = require('gulp-sourcemaps');
 const merge = require('merge2');
 
@@ -20,11 +21,19 @@ gulp.task('ts:build', ['clean'], () => {
   const tsProject = ts.createProject('./tsconfig.json', {
     declaration: true,
   });
-  
+
   const tsResult = tsProject.src()
                             .pipe(ts(tsProject));
   return merge([
-    tsResult.js.pipe(gulp.dest('./lib')),
-    tsResult.dts.pipe(gulp.dest('./lib'))
+    tsResult.js
+            .pipe(gulp.dest((file) => {
+              file.path = file.path.replace('src', '');
+              return './lib';
+            })),
+    tsResult.dts
+            .pipe(gulp.dest((file) => {
+              file.path = file.path.replace('src', '');
+              return './lib';
+            })),
     ]);
 });
---
+++ tests/setup.js/tests/setup.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.config = {
+    namespace: 'cwwebeventprocessor',
+    hubname: 'cwwebeventprocessor',
+    name: 'jslib',
+    key: 'zBZG3JSAaCmK1APNJVkwqTvuhcuYyCILqJO1Rae3l7w='
+};
